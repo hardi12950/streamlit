997d26ae5967ac09f13629e13b8e2bc07f25287c
hardi12950 <166913922+hardi12950@users.noreply.github.com>
2024-04-13 20:25:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 # Get your OpenAI API key from environment variables
 
 # api_key = os.getenv("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")  # Used in production - Uncomment this line when you deploy
-# api_key = "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" # Used in development - Delete this line when you deploy - This api_key is only made for demonstration purposes
+api_key = "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" # Used in development - Delete this line when you deploy - This api_key is only made for demonstration purposes
 
 # Cell 2: Title & Description
 st.title('🤖 AI Data Interview Assistant')
